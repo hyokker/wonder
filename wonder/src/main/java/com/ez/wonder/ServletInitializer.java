f9e04aa1b5078344d214d49280ae357121a1dde0
LeeJ1Hyun <nextlinehappy516@gmail.com>
2022-06-10 17:34:26 +0900

    첫번째 커밋 by lee

---
+++ wonder/src/main/java/com/ez/wonder/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ez.wonder;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WonderApplication.class);
+	}
+
+}
---
+++ wonder/src/test/java/com/ez/wonder/WonderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ez.wonder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
