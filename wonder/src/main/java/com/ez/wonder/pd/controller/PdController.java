0544f0098ab43dbfdd7544938d7285b40395d56f
hyunsooyk <107205330+hyunsooyk@users.noreply.github.com>
2022-06-10 18:14:56 +0900

    PdController 생성

---
+++ wonder/src/main/java/com/ez/wonder/pd/controller/PdController.java
@@ -0,0 +1,11 @@
+package com.ez.wonder.pd.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PdController {
+	public String test() {
+		System.out.println("dsdf");
+		return "test";
+	}
+}
