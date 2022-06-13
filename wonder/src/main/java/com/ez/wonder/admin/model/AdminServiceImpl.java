391e81253a5236c079e530aee46442410790cfdd
hyokker <105181325+hyokker@users.noreply.github.com>
2022-06-13 17:26:30 +0900

    admin.xml, AdmonController,AdminServiceImpl 주석처리 main.jsp, top.jsp 원더 로고 클릭시 메인페이지 이동, wonder? 클릭시 소개페이지 이동 소개페이지 내용 작성 진행중

--- wonder/src/main/java/com/ez/wonder/admin/model/AdminServiceImpl.java
+++ wonder/src/main/java/com/ez/wonder/admin/model/AdminServiceImpl.java
@@ -1,50 +1,50 @@
-package com.ez.wonder.admin.model;
-
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
-import com.ez.wonder.common.SearchVO;
-import com.ez.wonder.member.model.MemberVO;
-
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-public class AdminServiceImpl implements AdminService{
-	private static final Logger logger
-		= LoggerFactory.getLogger(AdminServiceImpl.class);
-	
-	private final AdminDAO adminDao;
-	
-	@Override
-	public List<MemberVO> selectMember(SearchVO searchVo){
-		return adminDao.selectMember(searchVo);
-	}
-	
-	@Override
-	public int getTotalRecord(SearchVO searchVo) {
-		return adminDao.getTotalRecord(searchVo);
-	}
-	
-	public int checkPwd(String adminPwd) {
-		String dbPwd = (String) session.getAttribute("adminPwd");
-		
-		int result = 0;
-		if(dbPwd!=null && !dbPwd.isEmpty()) {
-			if(dbPwd.equals(adminPwd)) {
-				result = AdminService.AGREE_PWD;
-			}else {
-				result = AdminService.DISAGREE_PWD;
-			}
-		}
-		return result;
-	}
-	
-	@Override
-	public int updatePwd(String adminPwd) {
-		return adminDao.upadtePwd(adminPwd);
-	}
-}
+//package com.ez.wonder.admin.model;
+//
+//import java.util.List;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Service;
+//
+//import com.ez.wonder.common.SearchVO;
+//import com.ez.wonder.member.model.MemberVO;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class AdminServiceImpl implements AdminService{
+//	private static final Logger logger
+//		= LoggerFactory.getLogger(AdminServiceImpl.class);
+//	
+//	private final AdminDAO adminDao;
+//	
+//	@Override
+//	public List<MemberVO> selectMember(SearchVO searchVo){
+//		return adminDao.selectMember(searchVo);
+//	}
+//	
+//	@Override
+//	public int getTotalRecord(SearchVO searchVo) {
+//		return adminDao.getTotalRecord(searchVo);
+//	}
+//	
+//	public int checkPwd(String adminPwd) {
+//		String dbPwd = (String) session.getAttribute("adminPwd");
+//		
+//		int result = 0;
+//		if(dbPwd!=null && !dbPwd.isEmpty()) {
+//			if(dbPwd.equals(adminPwd)) {
+//				result = AdminService.AGREE_PWD;
+//			}else {
+//				result = AdminService.DISAGREE_PWD;
+//			}
+//		}
+//		return result;
+//	}
+//	
+//	@Override
+//	public int updatePwd(String adminPwd) {
+//		return adminDao.upadtePwd(adminPwd);
+//	}
+//}
