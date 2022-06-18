fedc73cf8c5eeda692c03779d628d1dcda266244
LeeJ1Hyun <nextlinehappy516@gmail.com>
2022-06-18 18:05:56 +0900

    Update PdDetailVO.java

--- wonder/src/main/java/com/ez/wonder/pd/model/PdDetailVO.java
+++ wonder/src/main/java/com/ez/wonder/pd/model/PdDetailVO.java
@@ -3,17 +3,18 @@ package com.ez.wonder.pd.model;
 import lombok.Data;
 
 @Data
-public class PdDetailVO {
-	private int detailNo;
-	private int pdNo;
+public class PddetailVO {
+	private int detailNo ;
+	private int pdNo  ;
 	private String pdType;
-	private int pdPrice;
-	private int pdTerm;
-	private int editCount;
-	private String serviceExplanation;
-	private String editExplanation;
-	private String commercial;
-	private String codeSupply;
-	private String payment;
-	private String customize;
+	private int pdPrice ;
+	private int pdTerm ;
+	private int editCount; 
+	private String serviceExplanation ; 
+	private String editExplanation ; 
+	private String commercial ; 
+	private String codeSupply ; 
+	private String payment ;
+	private String customize; 
+	
 }
