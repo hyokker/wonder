52f9a81572f970bc8cdcc3cb3bb13c50784541f0
SeoWk <105574739+SeoWk@users.noreply.github.com>
2022-06-20 18:19:11 +0900

    서원기 commit 완료

--- wonder/src/main/java/com/ez/wonder/board/controller/BoardController.java
+++ wonder/src/main/java/com/ez/wonder/board/controller/BoardController.java
@@ -1,5 +1,11 @@
 package com.ez.wonder.board.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -11,9 +17,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ez.wonder.board.model.BoardService;
 import com.ez.wonder.board.model.BoardVO;
+import com.ez.wonder.common.ConstUtil;
+import com.ez.wonder.common.FileUploadUtil;
+import com.ez.wonder.common.PaginationInfo;
 import com.ez.wonder.common.SearchVO;
 
 import lombok.RequiredArgsConstructor;
@@ -23,7 +33,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BoardController {
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
 	private final BoardService boardService;
+	private final FileUploadUtil fileUploadUtil;
 	
 	@GetMapping("/test")
 	public void test() {
@@ -40,6 +52,31 @@ public class BoardController {
 			HttpServletRequest request) {
 		logger.info("글쓰기 처리, 파라미터 vo={}",vo);
 		
+		//파일 업로드 처리
+		String fileName="", oFileName="";
+		long fileSize=0;
+		
+		try {
+			List<Map<String, Object>> fileList
+			=fileUploadUtil.fileUpload(request,
+					ConstUtil.UPLOAD_FILE_FLAG);
+			
+			for(Map<String, Object> fileMap : fileList) {
+				oFileName=(String) fileMap.get("originalFileName");
+				fileName=(String) fileMap.get("fileName");
+				fileSize= (long) fileMap.get("fileSize");				
+			}//for
+			
+			logger.info("파일업로드 성공! fileName={}, fileSize={}", fileName, fileSize);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		vo.setFileName(fileName);
+		vo.setOriginalFileName(oFileName);
+		vo.setFileSize(fileSize);
+		
 		int cnt=boardService.insertBoard(vo);
 		logger.info("글쓰기 처리 결과, cnt={}", cnt);
 		
@@ -47,69 +84,184 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/list")
-	public String list(@ModelAttribute SearchVO searchVo, Model model) {
+	public void list(@ModelAttribute SearchVO searchVo, Model model) {
 		logger.info("자유게시판 목록, 파라미터 searchVo={}", searchVo);
 		
-		//[1] pagingInfo
-		/*
-		 * PaginationInfo pagingInfo = new PaginationInfo();
-		 * pagingInfo.setBlockSize(ConstUtil.BLOCKSIZE);
-		 * pagingInfo.setRecordCountPerPage(ConstUtil.RECORD_COUNT);
-		 * pagingInfo.setCurrentPage(searchVo.getCurrentPage());
-		 * 
-		 * //[2] searchVo.set
-		 * searchVo.setFirstRecordIndex(pagingInfo.getFirstRecordIndex());
-		 * searchVo.setRecordCountPerPage(ConstUtil.RECORD_COUNT);
-		 */
 		
-		//List<BoardVO> list=boardService.selectAll(searchVo);
-		//logger.info("글 목록 조회 결과, list.size={}", list.size());
+		 PaginationInfo pagingInfo = new PaginationInfo();
+		pagingInfo.setBlockSize(ConstUtil.BLOCKSIZE);
+		 pagingInfo.setRecordCountPerPage(ConstUtil.RECORD_COUNT);
+		  pagingInfo.setCurrentPage(searchVo.getCurrentPage());
+		 
+		  searchVo.setFirstRecordIndex(pagingInfo.getFirstRecordIndex());
+		  searchVo.setRecordCountPerPage(ConstUtil.RECORD_COUNT);
+		 
 		
-		//totalRecord개수 구하기
-		//int totalRecord=boardService.getTotalRecord(searchVo);
-		//logger.info("글목록 totalRecord={}", totalRecord);
+		List<BoardVO> list=boardService.selectAll(searchVo);
+		logger.info("조회 결과, list.size={}", list.size());
 		
-		//pagingInfo.setTotalRecord(totalRecord);
+		int totalRecord=boardService.getTotalRecord(searchVo);
+		logger.info("조회 건수 totalRecord={}", totalRecord);
 		
-		//model.addAttribute("list", list);
-		//model.addAttribute("pagingInfo", pagingInfo);
+		pagingInfo.setTotalRecord(totalRecord);
 		
-		return "board/list";
+		model.addAttribute("list", list);
+		model.addAttribute("pagingInfo", pagingInfo);
 	}
 	
 	@RequestMapping("/countUpdate")
-	public String countUpdate(@RequestParam(defaultValue = "0") int no,
-			Model model) {
-		return "redirect:/board/detail?no="+no;
+	public String countUpdate(@RequestParam(defaultValue = "0") int boardNo, Model model) {
+		logger.info("조회수 증가, 파라미터 boardNo={}", boardNo);
+		
+		if(boardNo==0) {
+			model.addAttribute("msg", "잘못된 url 접근입니다.");
+			model.addAttribute("url", "/board/list");
+			return "/common/message";
+		}
+		
+		int cnt=boardService.updateCount(boardNo);
+		logger.info("조회수 증가 결과, cnt={}", cnt);
+		
+		return "redirect:/board/detail?boardNo="+boardNo;
 	}
 	
 	@RequestMapping("/detail")
-	public String detail(@RequestParam(defaultValue = "0") int no,
-			Model model) {
+	public String detail(@RequestParam(defaultValue = "0") int boardNo,
+			HttpServletRequest request, Model model) {
+		logger.info("게시글 상세보기 파라미터 boardNo={}", boardNo);
+
+		if(boardNo==0) {
+			model.addAttribute("msg", "잘못된 url 접근입니다");
+			model.addAttribute("url", "/board/list");
+			return "/common/message";
+		}
+
+		BoardVO vo=boardService.selectByNo(boardNo);
+		logger.info("상세보기결과, vo={}", vo);
+
+		String fileInfo
+		=fileUploadUtil.getFileInfo(vo.getOriginalFileName(), 
+				vo.getFileSize(), request);
+
+		model.addAttribute("vo", vo);
+		model.addAttribute("fileInfo", fileInfo);
+		
 		return "board/detail";
 		
 	}
 	
+	@RequestMapping("/download")
+	public ModelAndView download(@RequestParam(defaultValue = "0") int boardNo, 
+			@RequestParam String fileName, HttpServletRequest request) {
+		
+		logger.info("다운로드 처리, 파라미터 boardNo={}, fileName={}", boardNo, fileName);
+
+		int cnt=boardService.updateDownCount(boardNo);
+		logger.info("다운로드 수 증가 결과, cnt={}", cnt);
+
+		Map<String, Object> map = new HashMap<>();
+		String uploadPath = fileUploadUtil.getUploadPath(request, 
+				ConstUtil.UPLOAD_FILE_FLAG);
+		File file = new File(uploadPath, fileName);
+		map.put("file", file);
+
+		ModelAndView mav = new ModelAndView("BoardDownloadView", map);
+
+		return mav;
+	}
+	
 	@GetMapping("/edit")
-	public String edit_get(@RequestParam(defaultValue = "0") int no,
+	public String edit_get(@RequestParam(defaultValue = "0") int boardNo,
 			Model model) {
+		logger.info("글 수정 페이지, 파라미터 no = {}", boardNo);
+
+		if(boardNo == 0) {
+			model.addAttribute("msg", "잘못된 접근입니다");
+			model.addAttribute("url", "/board/list");
+			return "/common/message";
+		}
+
+		BoardVO vo = boardService.selectByNo(boardNo);
+		logger.info("수정할 글 상세보기 vo={}", vo);
+
+		model.addAttribute("vo", vo);
+
 		return "board/edit";
 	}
 	
 	@PostMapping("/edit")
-	public String edit_post(@ModelAttribute BoardVO vo, Model model){
+	public String edit_post(@ModelAttribute BoardVO vo,
+			@RequestParam String oldFileName,
+			HttpServletRequest request, Model model) {
+		logger.info("수정 처리, 파라미터 vo={}", vo);
+		
+		String msg="비밀번호 체크 실패!", url="/board/edit?boardNo="+vo.getBoardNo();
+		if(boardService.checkPwd(vo.getBoardNo(), vo.getPwd())) {
+			//
+			String fileName="", originalFileName="";
+			long fileSize=0;
+			List<Map<String, Object>> fileList=null;
+			try {
+				fileList
+				=fileUploadUtil.fileUpload(request, ConstUtil.UPLOAD_FILE_FLAG);
+				for(Map<String, Object> fileMap: fileList) {
+					fileName=(String) fileMap.get("fileName");
+					originalFileName=(String) fileMap.get("originalFileName");
+					fileSize=(long) fileMap.get("fileSize");
+				}//for
+				
+				logger.info("수정 처리-파일 업로드 성공!");
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+					e.printStackTrace();
+			}
+		
+			vo.setFileName(fileName);
+			vo.setOriginalFileName(originalFileName);
+			vo.setFileSize(fileSize);
+			//
+			int cnt=boardService.updateBoard(vo);
+			logger.info("글 수정 처리 결과, cnt={}", cnt);
+
+			if(cnt>0) {
+				msg="수정되었습니다.";
+				url="/board/detail?boardNo="+vo.getBoardNo();
+				
+				if(!fileList.isEmpty()) {	//새로 파일 첨부한 경우
+					if(oldFileName!=null && !oldFileName.isEmpty()) {
+						//기존 파일이 있는 경우
+						String uploadPath
+						=fileUploadUtil.getUploadPath(request, ConstUtil.UPLOAD_FILE_FLAG);
+						File oldFile = new File(uploadPath, oldFileName);
+						if(oldFile.exists()) {
+							boolean bool=oldFile.delete();
+							logger.info("글수정-파일 삭제여부 : {}", bool);
+						}
+					}
+				}
+			}else {
+				msg="글 수정 실패";
+			}
+		}else {
+			msg="비밀번호 불일치!";
+		}
+		
+		model.addAttribute("msg", msg);
+		model.addAttribute("url", url);
+		
 		return "common/message";
 	}
 	
 	
 	@GetMapping("/delete")
-	public String delete_get(@RequestParam(defaultValue = "0") int no,
+	public String delete_get(@RequestParam(defaultValue = "0") int boardNo,
 			@ModelAttribute BoardVO vo, Model model) {
 		return "board/delete";
 	}
 	
 	@PostMapping("/delete")
-	public String delete_post(@RequestParam(defaultValue = "0")int no,
+	public String delete_post(@RequestParam(defaultValue = "0")int boardNo,
 			@RequestParam String pwd, Model model) {
 		return "common/message";
 	}
--- wonder/src/main/java/com/ez/wonder/board/model/BoardServiceImpl.java
+++ wonder/src/main/java/com/ez/wonder/board/model/BoardServiceImpl.java
@@ -1,26 +1,76 @@
 package com.ez.wonder.board.model;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ez.wonder.common.SearchVO;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService{
 	private static final Logger logger
 		=LoggerFactory.getLogger(BoardServiceImpl.class);
+		
+	private final BoardDAO boardDao;
 	
-	@Autowired
-	private BoardDAO boardDao;
-
-	public BoardServiceImpl() {
-		logger.info("생성자 호출 : BoardServiceImpl");
-	}
-
 	@Override
 	public int insertBoard(BoardVO vo) {
 		return boardDao.insertBoard(vo);
 	}
+
+	@Override
+	public List<BoardVO> selectAll(SearchVO searchVo) {
+		return boardDao.selectAll(searchVo);
+	}
+
+	@Override
+	public int getTotalRecord(SearchVO searchVo) {
+		return boardDao.getTotalRecord(searchVo);
+	}
+
+	@Override
+	public int updateCount(int boardNo) {
+		return boardDao.updateCount(boardNo);
+	}
+
+	@Override
+	public BoardVO selectByNo(int boardNo) {
+		return boardDao.selectByNo(boardNo);
+	}
+
+	@Override
+	public int updateDownCount(int boardNo) {
+		return boardDao.updateDownCount(boardNo);
+	}
+
+	@Override
+	public boolean checkPwd(int boardNo, String pwd) {
+		String dbPwd=boardDao.selectPwd(boardNo);
+		
+		boolean result=false;
+		if(dbPwd.equals(pwd)){
+			result=true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public int updateBoard(BoardVO vo) {
+		return boardDao.updateBoard(vo);
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
