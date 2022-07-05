7bfe4ccb7df0bac1df68efc7b59f2e38e7d4994c
LeeJ1Hyun <nextlinehappy516@gmail.com>
2022-07-06 02:26:19 +0900

    admin memberList 페이징, 검색, 조건 기능구현 완료, 커스텀 el 메서드 생성, 적용(수정사항 존재) by hyun

--- wonder/src/main/java/com/ez/wonder/common/Utility.java
+++ wonder/src/main/java/com/ez/wonder/common/Utility.java
@@ -19,47 +19,47 @@ public class Utility {
 		return result;
 	}
 
-	public static String lastVisit(HttpServletRequest request, HttpServletResponse response){
+	public static String lastVisit(HttpServletRequest request, HttpServletResponse response) {
 		Date now = new Date();
-		
+
 		MemberVO memberVo = new MemberVO();
 		Cookie cookie = new Cookie(memberVo.getUserId(), memberVo.getPwd());
-		cookie.setMaxAge(265*24*60*60);
+		cookie.setMaxAge(265 * 24 * 60 * 60);
 		response.addCookie(cookie);
-		
+
 		String msg = "";
 		boolean find = false;
-		Cookie lastVisit = null; //lastVisit 저장하고 있는 쿠키를 저장할 객체
-		
+		Cookie lastVisit = null; // lastVisit 저장하고 있는 쿠키를 저장할 객체
+
 		String nowVisit = "" + System.currentTimeMillis();
 		Cookie[] cookies = request.getCookies();
-		
-		if(cookies !=null) {
-			for(int i=0;i<cookies.length;i++) {
+
+		if (cookies != null) {
+			for (int i = 0; i < cookies.length; i++) {
 				lastVisit = cookies[i];
-				
-				if(lastVisit.getName().equals("lastvisitCookie")) {
+
+				if (lastVisit.getName().equals("lastvisitCookie")) {
 					find = true;
 					break;
 				}
 			}
 		}
-		
-		if(!find) {
+
+		if (!find) {
 			msg = "첫방문";
 			lastVisit = new Cookie("lastvisitCookie", nowVisit);
-			lastVisit.setMaxAge(265*24*60*60);
-			lastVisit.setPath("/"); //main으로 바꿀 예정
+			lastVisit.setMaxAge(265 * 24 * 60 * 60);
+			lastVisit.setPath("/"); // main으로 바꿀 예정
 			response.addCookie(lastVisit);
-		}else {
+		} else {
 			long diffDays;
 			long preVisit = new Long(lastVisit.getValue()).longValue();
 			long diff = now.getTime() - preVisit;
 			diff = diff / 1000; // 초 단위
-			
+
 			lastVisit.setValue(nowVisit);
 			response.addCookie(lastVisit);
-			
+
 			if (86400 < diff) {
 				diffDays = diff / (24 * 60 * 60);
 				msg = diffDays + "일 전";
@@ -72,12 +72,35 @@ public class Utility {
 			} else if (60 > diff) {
 				msg = diff + "초 전";
 			}
-			
+
 			return msg;
 		}
-		
+
 		return msg;
-		
+
 	}
-	
+
+	public static String diffOfDate(Date regdate) throws Exception {
+		Date now = new Date();
+		long diffDays;
+		String result = "";
+
+		long diff = now.getTime() - regdate.getTime();
+		diff = diff / 1000; // 초 단위
+
+		if (86400 < diff) {
+			diffDays = diff / (24 * 60 * 60);
+			result = diffDays + "일 전";
+		} else if (86400 > diff && diff >= 3600) {
+			diffDays = diff / (60 * 60);
+			result = diffDays + "시간 전";
+		} else if (60 <= diff && diff < 3600) {
+			diffDays = diff / (60);
+			result = diffDays + "분 전";
+		} else if (60 > diff) {
+			result = diff + "초 전";
+		}
+		return result;
+	}
+
 }
